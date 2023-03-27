d135ae62296ef82ec7e7b311415685b3eedac09f
Jackson Socolofsky <jtsoco@gmail.com>
2023-03-27 21:00:12 +0900

    added policies for bookmarks

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -5,8 +5,19 @@ class BookmarksController < ApplicationController
   end
 
   def create
+    @bookmark = Bookmark.new()
+    # ToDo, make sure that the params will be :spot_id and not :id
+    @spot = Spot.find(params[:spot_id])
+    @bookmark.user = current_user
+    @bookmark.spot = @spot
+    authorize @bookmark
+    @bookmark.save
   end
 
   def delete
+    @bookmark = Bookmark.find(params[:id])
+    authorize @bookmark
   end
+
+
 end
--- app/policies/bookmark_policy.rb
+++ app/policies/bookmark_policy.rb
@@ -1,8 +1,16 @@
 class BookmarkPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      scope.where(:user, user)
+    end
+  end
+
+  def create?
+    true
+  end
+
+  def destroy?
+    record.user == user
   end
 end
