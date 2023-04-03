7039fc49ba0f3e67df14847fccd675f71f6b2360
Vigoluca <lvigotti@gmail.com>
2023-04-03 15:48:54 +0900

    changed header position

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = "dwufxkqdp"
+  config.api_key = "673999264861667"
+  config.api_secret = "7ndoddWyb-mq7Vet9nvLBAKfoz4"
+  config.secure = true
+end
