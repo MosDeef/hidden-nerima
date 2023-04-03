b4ed38c76ee3f1041ca9fd05b3be6875990b0cda
Jackson Socolofsky <jtsoco@gmail.com>
2023-04-03 15:23:40 +0900

    dependents of user and spots are destroyed when they are

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :reviews
-  has_many :bookmarks
-  has_many :spots
+  has_many :bookmarks, dependent: :destroy
+  has_many :spots, dependent: :destroy
   validates :first_name, :last_name, presence: true
 end
