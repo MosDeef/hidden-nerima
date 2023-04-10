878295661d56d4874a0149d423335f18c72de2b3
Jackson Socolofsky <jtsoco@gmail.com>
2023-04-10 14:58:04 +0900

    added minimum twenty character content length to validations of review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,5 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :spot
   validates :user, :spot, :comment, :rating, presence: true
+  validates :content, length: {minimum: 20}
 end
