fd513a1d727f9b2b355a950670c0927db78fb866
Jackson Socolofsky <jtsoco@gmail.com>
2023-04-10 14:37:04 +0900

    removed extra has_one in review.rb and bookmark.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,17 @@
 class ReviewsController < ApplicationController
+  def create
+    @spot = Spot.find(params[:spot_id])
+    @review = Review.new(review_params)
+    @review.spot = @spot
+    @review.user = current_user
+    if @review.save
+      redirect_to spot_path(@spot)
+    else
+      render "spots/show", status: :unprocessable_entity
+    end
+  end
+
+  def review_params
+    params.require(:review).permit(:comment, :rating)
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,4 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :spot
   validates :user, :spot, :comment, :rating, presence: true
-  has_one :user
-  has_one :spot
 end
