45acd8f971e865140b27488ba8f10f1bee427364
Jackson Socolofsky <jtsoco@gmail.com>
2023-04-10 15:22:07 +0900

    added posted_at method to reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,8 @@ class Review < ApplicationRecord
   belongs_to :spot
   validates :user, :spot, :comment, :rating, presence: true
   validates :content, length: {minimum: 20}
+
+  def posted_time
+    "#{created_at.getlocal.day}/#{created_at.getlocal.month}/#{created_at.getlocal.year} at #{created_at.getlocal.hour}:#{created_at.getlocal.min}"
+  end
 end
