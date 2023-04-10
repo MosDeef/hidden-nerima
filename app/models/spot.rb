4e94f074c15b3face89160c91b620dd285195181
Jackson Socolofsky <jtsoco@gmail.com>
2023-04-10 14:54:17 +0900

    added dependent destroy to spot and user rb files

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,7 +1,7 @@
 class Spot < ApplicationRecord
   validates :name, :sub_category, :location, :description, :category, :user, presence: true
   belongs_to :user
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
   has_many_attached :photos
   enum category: [:location, :food, :experience, :transportation]
