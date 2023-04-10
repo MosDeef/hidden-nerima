9e9513fb9b1a3defbbe98c881e1637f5c14c90a2
Jackson Socolofsky <jtsoco@gmail.com>
2023-04-10 17:14:25 +0900

    creates a review with json post, still doesn't add to the list without refreshing

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,9 +6,15 @@ class ReviewsController < ApplicationController
     @review.user = current_user
     authorize @review
     if @review.save
-      redirect_to spot_path(@spot)
+      respond_to do
+        redirect_to spot_path(@spot)
+      end
+      format.json
     else
-      render "spots/show", status: :unprocessable_entity
+      respond_to do |html|
+        render "spots/show", status: :unprocessable_entity
+      end
+      format.json
     end
   end
 
