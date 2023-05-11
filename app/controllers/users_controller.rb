632c4d1622f6edf67f1f1c77ca6170456c96e971
Jackson Socolofsky <jtsoco@gmail.com>
2023-05-11 15:16:25 +0900

    made sure that if no bookmarks or user spots exist, the section of the profile page doesn't appear

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @my_spots = @user.spots
+    @my_bookmarks = @user.bookmarks
     authorize @user
   end
 
