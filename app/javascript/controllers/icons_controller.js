4063a36bd0ef2061c1ff10bde3aafe1378ad8681
Jackson Socolofsky <jtsoco@gmail.com>
2023-05-11 16:31:07 +0900

    bookmark works with simple form

--- app/javascript/controllers/icons_controller.js
+++ app/javascript/controllers/icons_controller.js
@@ -8,12 +8,12 @@ export default class extends Controller {
   }
   toggleclass() {
 
-    var element = document.getElementById("bookmark");
+    // var element = document.getElementById("bookmark");
 
-    // element.style.color = "#ff0000";
-    element.classList.toggle("fa-regular");
-    element.classList.toggle("fa-solid");
-    // element.addEventListener('click', this.toggleclass)
+    // // element.style.color = "#ff0000";
+    // element.classList.toggle("fa-regular");
+    // element.classList.toggle("fa-solid");
+    // // element.addEventListener('click', this.toggleclass)
   }
-  bookmarkIcon = document.getElementById("bookmark").addEventListener('click', this.toggleclass)
+  // bookmarkIcon = document.getElementById("bookmark").addEventListener('click', this.toggleclass)
 }
